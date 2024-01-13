2e862e2136462f5aecbe87b168d5ceb20c190370
ViplaoItanakr <viplao.itankar@getyolo.in>
2024-01-13 18:07:06 +0530

    updated initial home page

--- components/FAQ/Hero.js
+++ components/FAQ/Hero.js
@@ -5,7 +5,7 @@ const Hero = () => {
     return (
         <div className='flex justify-center items-center w-full mb-36 mt-10'>
             <div className="flex flex-col items-center gap-6 justify-center">
-                <h5 className='text-white text-center font-poppins xl:text-lg text-base font-normal leading-[170%] tracking-[2px] uppercase'>FAQ'S</h5>
+                <h5 className='text-white text-center font-poppins xl:text-lg text-base font-normal leading-[170%] tracking-[2px] uppercase'>FAQ&apos;S</h5>
                 <h1 className='text-[#F8F8F8] text-center font-poppins xl:text-[44px] text-3xl font-semibold leading-[150%]'>
                     Struggling in <br className='xs:block hidden' />knowing us
                 </h1>
--- components/FAQ/MoreFaqs.js
+++ components/FAQ/MoreFaqs.js
@@ -20,10 +20,10 @@ const MoreFaqs = () => {
                 <div className='md:mx-28 flex flex-col items-start gap-10 my-14 xl:px-0 px-6'>
                     <div className="flex max-w-[505px] flex-col justify-center items-center gap-4">
                         <h2 className='text-white font-poppins text-[40px] xs:text-[30px] font-semibold leading-normal tracking-[-0.165px]'>
-                            Can't find what you&apos;re looking for?
+                            Can&apos;t find what you&apos;re looking for?
                         </h2>
                         <p className="font-poppins text-[#878787] text-lg font-normal leading-[150%]">
-                            We're here to help 24 hours a day, 7 days a week. Holiday hours may apply.
+                            We&apos;re here to help 24 hours a day, 7 days a week. Holiday hours may apply.
                         </p>
                     </div>
                     <div className='flex items-start gap-5'>
