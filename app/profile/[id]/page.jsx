a5b9aa1954251d192779331b35c7466d58655f25
Viplao Itankar <viplao.itankar@getyolo.in>
2024-03-19 13:28:54 +0530

    Contact Sharing

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -24,7 +24,7 @@ const Profile = ({ params }) => {
       }).catch(err => {
         console.log(err)
       }).finally(() => {
-        setTimeout(()=>{
+        setTimeout(() => {
           setIsLoading(false);
         }, 1000)
       })
@@ -45,72 +45,86 @@ const Profile = ({ params }) => {
     data !== null ?
       (
         <div className="flex justify-center items-center w-screen">
-          <div className="flex flex-col justify-center items-center my-5 lg:w-[50%] md:w-[60%] sm:w-[75%] w-[90%] gap-4" >
+          <div className="flex flex-col justify-center items-center my-5 lg:w-[50%] md:w-[60%] sm:w-[75%] w-[90%] gap-4 max-w-[600px]" >
+            <div className="share-bg absolute h-[200px] top-0 lg:w-[50%] md:w-[60%] sm:w-[75%] w-[90%] gap-4 max-w-[600px]" />
             <div className='card-round' >
               <Image src={data.profile} alt='profile' width={100} height={100} className='rounded-full p-1' />
             </div>
             <p className='text-[#ffffff] text-[25px] font-semibold' >{data.name}</p>
             {
               data.visibility.bio && (
-                <p className='text-[#ffffff50]' >{data.bio}</p>
+                <div>
+                  <p className='text-[#ffffff] text-[20px] text-center' >about me</p>
+                  <p className='text-[#ffffff50] text-center' >{data.bio}</p>
+                </div>
               )
             }
-            {
-              data.visibility.phoneNumber && (
-                <Link href={`tel:${data.phoneNumber}`} className='button w-full text-[#ffffff] flex gap-2 justify-center items-center' >
-                  <FiPhone />
-                  <button>phone number</button>
-                </Link>
-              )
-            }
-            {
-              data.visibility.emailId && (
-                <Link href={`mailto:${data.emailId}`} className='button w-full text-[#ffffff] flex gap-2 justify-center items-center' >
-                  <MdOutlineEmail />
-                  <button>Email</button>
-                </Link>
-              )
-            }
+            <div>
+              <p className='text-[#ffffff50] text-center mt-2' >LINKS</p>
+              <p className='text-[#ffffff] text-[25px] font-semibold text-center leading-7' >let&apos;s connect on<br />the social media</p>
+            </div>
             <div className='flex flex-row gap-4 flex-wrap w-full items-center justify-around' >
               {
+                data.visibility.phoneNumber && (
+                  <Link href={`tel:${data.phoneNumber}`} className='px-3 share button sm:min-w-[30%] min-w-[40%] text-[#ffffff] flex-1 flex flex-col gap-2' >
+                    <FiPhone />
+                    <button>mobile</button>
+                  </Link>
+                )
+              }
+              {
+                data.visibility.emailId && (
+                  <Link href={`mailto:${data.emailId}`} className='px-3 share button sm:min-w-[30%] min-w-[40%] text-[#ffffff] flex-1 flex flex-col gap-2' >
+                    <MdOutlineEmail />
+                    <button>Email</button>
+                  </Link>
+                )
+              }
+              {
                 data.visibility.facebook && (
-                  <Link href={data.facebook} className='px-3 share button min-w-[25%] text-[#ffffff] flex-1 text-[35px]' >
-                    <TiSocialFacebookCircular />
+                  <Link href={data.facebook} className='px-3 share button sm:min-w-[30%] min-w-[40%] text-[#ffffff] flex-1 flex flex-col gap-2' >
+                    <p className='text-[30px]' ><TiSocialFacebookCircular /></p>
+                    <button>facebook</button>
                   </Link>
                 )
               }
               {
                 data.visibility.instagram && (
-                  <Link href={data.instagram} className='px-3 share button min-w-[25%] text-[#ffffff] flex-1 text-[35px]' >
+                  <Link href={data.instagram} className='px-3 share button sm:min-w-[30%] min-w-[40%] text-[#ffffff] flex-1 flex flex-col gap-2' >
                     <FaInstagram />
+                    <button>instagram</button>
                   </Link>
                 )
               }
               {
                 data.visibility.twitter && (
-                  <Link href={data.twitter} className='px-3 share button min-w-[25%] text-[#ffffff] flex-1 text-[35px]' >
+                  <Link href={data.twitter} className='px-3 share button sm:min-w-[30%] min-w-[40%] text-[#ffffff] flex-1 flex flex-col gap-2' >
                     <FaXTwitter />
+                    <button>twitter</button>
                   </Link>
                 )
               }
               {
                 data.visibility.whatsapp && (
-                  <Link href={data.whatsapp} className='px-3 share button min-w-[25%] text-[#ffffff] flex-1 text-[35px]' >
+                  <Link href={data.whatsapp} className='px-3 share button sm:min-w-[30%] min-w-[40%] text-[#ffffff] flex-1 flex flex-col gap-2' >
                     <FaWhatsapp />
+                    <button>whatsapp</button>
                   </Link>
                 )
               }
               {
                 data.visibility.snapchat && (
-                  <Link href={data.snapchat} className='px-3 share button min-w-[25%] text-[#ffffff] flex-1 text-[35px]' >
+                  <Link href={data.snapchat} className='px-3 share button sm:min-w-[30%] min-w-[40%] text-[#ffffff] flex-1 flex flex-col gap-2' >
                     <FaSnapchat />
+                    <button>snapchat</button>
                   </Link>
                 )
               }
               {
                 data.visibility.discord && (
-                  <Link href={data.discord} className='px-3 share button min-w-[25%] text-[#ffffff] flex-1 text-[35px]' >
+                  <Link href={data.discord} className='px-3 share button sm:min-w-[30%] min-w-[40%] text-[#ffffff] flex-1 flex flex-col gap-2' >
                     <RiDiscordLine />
+                    <button>discord</button>
                   </Link>
                 )
               }
