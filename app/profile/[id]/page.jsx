aab8053b2a2ca12a8978bd99164b35404adb753c
Viplao Itankar <viplao.itankar@getyolo.in>
2024-04-24 23:07:19 +0530

    education

--- app/profile/[id]/page.jsx
+++ app/profile/[id]/page.jsx
@@ -165,7 +165,7 @@ const Profile = ({ params }) => {
                 }
               </div>
               {
-                data.visibility.school && data.visibility?.education && (
+                (data.visibility.school || data.visibility?.education) && (
                   <div className='self-start' >
                     <p className='text-[#ffffff50] mt-2 text-[12px]' >EDUCATION</p>
                     <p className='text-[#ffffff] text-[18px] font-semibold' >let&apos;s know more<br />about me</p>
