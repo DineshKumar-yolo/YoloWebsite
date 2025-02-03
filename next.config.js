69fb973da79d2c68b85f5dd2d21002c78ca0d2c2
VIPLove <shivansh@VIPLove.local>
2025-02-03 15:07:44 +0530

    next remote image config changed

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      domains: ['user-data-images.s3.ap-south-1.amazonaws.com', 'example.com', 'ibb.co', 'user-data-images.s3.amazonaws.com', 'storage.googleapis.com'],
-    },
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '**',
+      },
+    ],
+    // domains: ['user-data-images.s3.ap-south-1.amazonaws.com', 'example.com', 'ibb.co', 'user-data-images.s3.amazonaws.com', 'storage.googleapis.com'],
+  },
 }
 output: "export",
-module.exports = nextConfig
+  module.exports = nextConfig
